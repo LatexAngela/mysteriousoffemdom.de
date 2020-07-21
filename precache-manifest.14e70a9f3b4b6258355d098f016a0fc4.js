5a92ef38528f020e11d1e9a503c9f9696ea17de7
LatexAngela <68613380+LatexAngela@users.noreply.github.com>
2020-07-21 23:57:12 +0300

    First Page Create !

---
+++ precache-manifest.14e70a9f3b4b6258355d098f016a0fc4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78e8495ac341ff177456b0bd46c0156",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d91b409ce368be3be78a",
+    "url": "./static/css/main.7a9bbf0f.chunk.css"
+  },
+  {
+    "revision": "337abcf052cd347f620c",
+    "url": "./static/js/2.9e62fc7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9e62fc7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91b409ce368be3be78a",
+    "url": "./static/js/main.0a230df3.chunk.js"
+  },
+  {
+    "revision": "ef12a46dce674971e973",
+    "url": "./static/js/runtime-main.23d61c58.js"
+  },
+  {
+    "revision": "7d51d4869652d1cf0f62ad718da3b1dd",
+    "url": "./static/media/ImageFirstPage.7d51d486.png"
+  },
+  {
+    "revision": "f68d7dfcf5fde9c100da6a1923e4904b",
+    "url": "./static/media/LogoforSite.f68d7dfc.png"
+  },
+  {
+    "revision": "76d89f21a030d8bb911a341291e5fb7c",
+    "url": "./static/media/Sansation-Light.76d89f21.ttf"
+  },
+  {
+    "revision": "8ddb79ec03116f946b71adfae83cd492",
+    "url": "./static/media/Solway-Medium.8ddb79ec.ttf"
+  }
+]);
