254becd9918dbcda02b975edc6b1742845a60f91
LatexAngela <68613380+LatexAngela@users.noreply.github.com>
2020-07-29 00:43:30 +0300

    Add files via upload
    Add photo page

---
+++ precache-manifest.41c5e74874629b09f65ddbf72fb27484.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49dddfc1beda53f55639c0ae507c907a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c08f945b2fb89fc099ae",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b0b979e69e27803babb5",
+    "url": "./static/css/main.6565e07f.chunk.css"
+  },
+  {
+    "revision": "c08f945b2fb89fc099ae",
+    "url": "./static/js/2.144784e0.chunk.js"
+  },
+  {
+    "revision": "16021066fd5e2548dbeed610083370ad",
+    "url": "./static/js/2.144784e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b979e69e27803babb5",
+    "url": "./static/js/main.72440819.chunk.js"
+  },
+  {
+    "revision": "2ed2adc6a6986a2a161f",
+    "url": "./static/js/runtime-main.a9b8f556.js"
+  },
+  {
+    "revision": "7d51d4869652d1cf0f62ad718da3b1dd",
+    "url": "./static/media/ImageFirstPage.7d51d486.png"
+  },
+  {
+    "revision": "f68d7dfcf5fde9c100da6a1923e4904b",
+    "url": "./static/media/LogoforSite.f68d7dfc.png"
+  },
+  {
+    "revision": "76d89f21a030d8bb911a341291e5fb7c",
+    "url": "./static/media/Sansation-Light.76d89f21.ttf"
+  },
+  {
+    "revision": "8ddb79ec03116f946b71adfae83cd492",
+    "url": "./static/media/Solway-Medium.8ddb79ec.ttf"
+  },
+  {
+    "revision": "115c3d3f1b1f2b5f004ada4bfeb17430",
+    "url": "./static/media/index_001.115c3d3f.jpg"
+  },
+  {
+    "revision": "9f1918c511b4c2edde20c5db3fb990ed",
+    "url": "./static/media/index_002.9f1918c5.jpg"
+  },
+  {
+    "revision": "a3697ef41bc93232da5abab5ad7eeb5c",
+    "url": "./static/media/index_003.a3697ef4.jpg"
+  },
+  {
+    "revision": "909d8c206edf443069e9a8f1383728fa",
+    "url": "./static/media/index_004.909d8c20.jpg"
+  },
+  {
+    "revision": "a03576912d137b264fe1cea87a0b1615",
+    "url": "./static/media/index_005.a0357691.jpg"
+  },
+  {
+    "revision": "d6d61f341f0f7cfadb387548e12a7faa",
+    "url": "./static/media/index_006.d6d61f34.jpg"
+  },
+  {
+    "revision": "9bf5570738d1f5859c260bb479c1e411",
+    "url": "./static/media/index_008.9bf55707.jpg"
+  },
+  {
+    "revision": "de8babac7eed92658780ff0abc331b42",
+    "url": "./static/media/index_009.de8babac.jpg"
+  }
+]);
